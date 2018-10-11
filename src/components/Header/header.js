193f634cec097e07cb3acb2bd5b5227f3fe71aea
moxdev <moxdev1@gmail.com>
2018-10-11 17:50:46 -0400

    setting up header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,38 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+import logo from '../../images/fhs-logo.svg'
 
-export default Header
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  padding: 1rem 1.875rem;
+  border-bottom: 2px solid #6f6259;
+
+  .logo {
+    width: 150px;
+  }
+
+  h1 {
+    color: #54565a;
+  }
+`
+
+export default class Header extends Component {
+  render() {
+    const { data } = this.props
+
+    return (
+      <HeaderWrapper>
+        <img className="logo" src={logo} alt="Franklin Handyman Logo" />
+
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          />
+          {data.site.siteMetadata.title}
+        </h1>
+      </HeaderWrapper>
+    )
+  }
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string.isRequired,
+  data: PropTypes.node.isRequired,
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header data={data} />
         <div
           style={{
             margin: '0 auto',
